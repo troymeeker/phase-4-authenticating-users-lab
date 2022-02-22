4f6eb17f1976b6c5796cae1e55235cee490d36dd
troymeeker <troymeeker1@gmail.com>
2022-02-22 12:08:24 -0800

    commit tree d0767bc90f225d46d5942c1598624a56617f475a

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user
+        else 
+            render json: {error: "Not authorized"}, status: :unauthorized
+        end
+    end
+end
+
+
+# Find a user in the database using the user id from the session hash
+# Return the user as a JSON object
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # resources :users, only: [:show]
+get "/me", to: "users#show"
+  # resources :sessions, only: [:create, :destroy]
+post "/login", to: "sessions#create"
+delete "/logout", to: "sessions#destroy"
 end
